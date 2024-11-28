4c637caa21427dc8822b54a5f3b83d8fd4d6ca39
erishBRBS <erishberboso10@gmail.com>
2024-11-28 17:39:38 +0800

    todo list

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todos";
+
+export const dummyData: Todo[] =[
+    {
+        id:1,
+        title: "Go learn React",
+        completed: false,
+    },
+    {
+        id:2,
+        title: "Go learn Laravel",
+        completed: false,
+    },
+    {
+        id:3,
+        title: "Rest 30minutes",
+        completed: false,
+    },
+];
